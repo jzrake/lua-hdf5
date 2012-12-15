62d1f29f07742368789e889b8593bb61fba49e80
Jonathan Zrake <jonathan.zrake@gmail.com>
2012-12-14 20:44:36 -0500

    Implement index method on file/group

--- hdf5.lua
+++ hdf5.lua
@@ -44,6 +44,8 @@ function BaseClass:close()
 end
 
 
+local hdf5 = { }
+
 --------------------------------------------------------------------------------
 -- HDF5 File class methods
 --------------------------------------------------------------------------------
@@ -56,21 +58,34 @@ local FileClass = inherit_from(BaseClass)
 local GroupClass = inherit_from(BaseClass)
 
 
-
-local FileMeta = inherit_from(BaseMeta)
-function FileMeta:__index(key)
-   return self._open_objects[key]
+local FileGroupSharedMeta = inherit_from(BaseMeta)
+function FileGroupSharedMeta:__index(key)
+if self._open_objects[key] then
+      return self._open_objects[key]
+   end
+   if not H5.H5Lexists(self._hid, key, hp0) then
+      return nil
+   end
+   local info = H5.new_H5O_info_t()
+   H5.H5Oget_info_by_name(self._hid, key, info, hp0)
+   if info.type == H5.H5O_TYPE_GROUP then
+      return hdf5.Group(self, key)
+   else
+      error("object %s/%s has an unsupported type", self._name, key)
+   end
 end
+
+local FileMeta = inherit_from(FileGroupSharedMeta)
 function FileMeta:__tostring()
    if self._hid ~= 0 then
-      return string.format("<HDF5 file: \"%s\" (mode %s)>", self._filename,
+      return string.format("<HDF5 file: \"%s\" (mode %s)>", self._name,
 			   self._mode)
    else
       return "<Closed HDF5 file>"
    end
 end
 
-local GroupMeta = inherit_from(BaseMeta)
+local GroupMeta = inherit_from(FileGroupSharedMeta)
 function GroupMeta:__tostring()
    if self._hid ~= 0 then
       return string.format("<HDF5 group: \"%s\">", self._name)
@@ -78,27 +93,24 @@ function GroupMeta:__tostring()
       return "<Closed HDF5 group>"
    end
 end
-function GroupMeta:__index(key)
-   return self._open_objects[key]
-end
 
-local function File(filename, mode)
+function hdf5.File(name, mode)
    -----------------------------------------------------------------------------
    -- HDF5 File constructor
    -----------------------------------------------------------------------------
    local new = { _type='file',
-		 _filename=filename,
+		 _name=name,
 		 _mode=mode,
 		 _hid=0,
 		 _close=H5.H5Fclose,
 		 _open_objects={ } }
    inherit_from(FileClass, new)
    if mode == "w" then
-      new._hid = H5.H5Fcreate(filename, H5.H5F_ACC_TRUNC, hp0, hp0)
+      new._hid = H5.H5Fcreate(name, H5.H5F_ACC_TRUNC, hp0, hp0)
    elseif mode == "r" then
-      new._hid = H5.H5Fopen(filename, H5.H5F_ACC_RDONLY, hp0)
+      new._hid = H5.H5Fopen(name, H5.H5F_ACC_RDONLY, hp0)
    elseif mode == "r+" then
-      new._hid = H5.H5Fopen(filename, H5.H5F_ACC_RDWR, hp0)
+      new._hid = H5.H5Fopen(name, H5.H5F_ACC_RDWR, hp0)
    else
       error("mode must be one of [w, r, r+]")
    end
@@ -107,7 +119,7 @@ local function File(filename, mode)
 end
 
 
-local function Group(parent, name)
+function hdf5.Group(parent, name)
    -----------------------------------------------------------------------------
    -- HDF5 Group constructor
    -----------------------------------------------------------------------------
@@ -118,7 +130,12 @@ local function Group(parent, name)
 		 _close=H5.H5Gclose,
 		 _open_objects={ } }
    inherit_from(GroupClass, new)
-   new._hid = H5.H5Gcreate2(parent._hid, name, hp0, hp0, hp0)
+   local exist = H5.H5Lexists(parent._hid, name, hp0)
+   if not exist then
+      new._hid = H5.H5Gcreate2(parent._hid, name, hp0, hp0, hp0)
+   else
+      new._hid = H5.H5Gopen2(parent._hid, name, hp0)
+   end
    parent._open_objects[name] = new
    setmetatable(new, GroupMeta)
    return new
@@ -126,20 +143,23 @@ end
 
 
 local function test1()
-   local h5f = File("outfile.h5", "w")
-   local h5g = Group(h5f, "thegroup")
-   local h5h = Group(h5g, "thesubgroup")
+   local h5f = hdf5.File("outfile1.h5", "w")
+   local h5g = hdf5.Group(h5f, "thegroup")
+   local h5h = hdf5.Group(h5g, "thesubgroup")
    assert(h5f["thegroup"]["thesubgroup"] == h5h)
 end
 
 
 local function test2()
-   local h5f = File("outfile.h5", "w")
-   local h5g = Group(h5f, "thegroup")
-   local h5h = Group(h5g, "thesubgroup")
-   h5h:close()
-   print(h5f["thegroup"]["thesubgroup"])
+   local h5f = hdf5.File("outfile2.h5", "w")
+   local h5g = hdf5.Group(h5f, "thegroup")
+   local h5h = hdf5.Group(h5g, "thesubgroup")
+   h5g:close()
+   assert(h5f["thegroup"]["thesubgroup"]._name == h5h._name)
+   assert(h5f["thething"] == nil)
 end
 
 test1()
---test2()
+test2()
+
+return hdf5
